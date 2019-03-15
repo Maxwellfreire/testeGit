ed58bf757d716c6a821633e9cc2ef52c998b340e
Maxwellfreire <43391418+Maxwellfreire@users.noreply.github.com>
2019-03-14 21:49:39 -0300

    Update testando.java

--- Git_teste/src/testando.java
+++ Git_teste/src/testando.java
@@ -3,6 +3,10 @@ public class testando {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Maxwell");
+		System.out.println("freire");
+
 
 	}
 
